a94f8041e970ef1f37482b666edcb81e12c69891
Ruslan <vich_91@mail.ru>
2024-01-11 23:16:46 +0500

    add style

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const searchInput = document.querySelector(".search__input");
 const searchBtn = document.querySelector(".search__btn");
-// const result = document.querySelector(".result");
 const formNode = document.querySelector(".search");
 
 const getInputValue = () => searchInput.value;
@@ -27,24 +26,26 @@ const getApiMovies = async (inputValue) => {
 const renderMovies = (data) => {
   const moviesEl = document.getElementById("movies");
   moviesEl.innerHTML = "";
+  console.log(moviesEl);
   data.Search.forEach((movie) => {
     const container = document.createElement("div");
     container.className = "render__div";
 
     const img = document.createElement("img");
     img.className = "render__img";
+    img.src = `${movie.Poster}`;
 
     const title = document.createElement("h1");
     title.className = "render__title";
-    title.innerText = `${movie.Title}`;
+    title.innerText = `Название: ${movie.Title}`;
 
     const year = document.createElement("p");
     year.className = "render__year";
-    year.innerText = `${movie.Year}`;
+    year.innerText = `Год: ${movie.Year}`;
 
-    const type = document.querySelector("p");
+    const type = document.createElement("p");
     type.className = "render__type";
-    type.innerText = `${movie.Type}`;
+    type.innerText = `Тип: ${movie.Type}`;
 
     moviesEl.appendChild(container);
     container.appendChild(img);
