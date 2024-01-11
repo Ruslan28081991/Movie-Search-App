2ab1f6cf747ba7dbcf8d82e78cdabb9fcdae200f
Ruslan <vich_91@mail.ru>
2024-01-11 17:57:59 +0500

    editing code

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const searchInput = document.querySelector(".search__input");
 const searchBtn = document.querySelector(".search__btn");
-const result = document.querySelector(".result");
+// const result = document.querySelector(".result");
+const formNode = document.querySelector(".search");
 
 const getInputValue = () => searchInput.value;
 
-const getMovies = () => {
+const getMovies = (e) => {
+  e.preventDefault(); // меняем стандартное поведение браузера
   const inputValue = getInputValue();
   getApiMovies(inputValue);
 };
-console.log(movies);
 
 const getApiMovies = async (inputValue) => {
   try {
@@ -17,32 +18,40 @@ const getApiMovies = async (inputValue) => {
     );
     const movies = await res.json();
     console.log(movies);
-    // renderMovies(movies);
+    renderMovies(movies);
   } catch (error) {
     console.log(error);
   }
 };
 
 const renderMovies = (data) => {
-  const container = document.createElement("div");
-  container.className = "render__div";
-  const img = document.createElement("img");
-  img.className = "render__img";
-  const title = document.createElement("h1");
-  title.className = "render__title";
-  // title.innerText = `${}`;
-  const year = document.createElement("p");
-  year.className = "render__year";
-  // year.innerText = `${}`;
-  const type = document.querySelector("p");
-  type.className = "render__type";
-  // type.innerText = `${}`;
+  const moviesEl = document.getElementById("movies");
+  moviesEl.innerHTML = "";
+  data.Search.forEach((movie) => {
+    const container = document.createElement("div");
+    container.className = "render__div";
 
-  result.appendChild(container);
-  container.appendChild(img);
-  container.appendChild(title);
-  container.appendChild(year);
-  container.appendChild(type);
+    const img = document.createElement("img");
+    img.className = "render__img";
+
+    const title = document.createElement("h1");
+    title.className = "render__title";
+    title.innerText = `${movie.Title}`;
+
+    const year = document.createElement("p");
+    year.className = "render__year";
+    year.innerText = `${movie.Year}`;
+
+    const type = document.querySelector("p");
+    type.className = "render__type";
+    type.innerText = `${movie.Type}`;
+
+    moviesEl.appendChild(container);
+    container.appendChild(img);
+    container.appendChild(title);
+    container.appendChild(year);
+    container.appendChild(type);
+  });
 };
 
-searchBtn.addEventListener("click", getMovies);
+formNode.addEventListener("submit", getMovies);
